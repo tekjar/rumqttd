ed6a69c28e6f3ea9474644ad14f2b7adf46bac4e
tekjar <k.teza1@gmail.com>
2017-04-16 19:38:17 +0530

    fixing 'decode' by returning Ok(None) during 0 len buf invokes when previous 'decode' read all the bytes in the stream

--- src/codec.rs
+++ src/codec.rs
@@ -1,8 +1,9 @@
 use std::io::{self, ErrorKind, Cursor};
+use std::error::Error;
 use bytes::BytesMut;
 use tokio_io::codec::{Encoder, Decoder};
 
-use mqtt3::{Error, Packet, MqttWrite, MqttRead};
+use mqtt3::{self, Packet, MqttWrite, MqttRead};
 
 pub struct MqttCodec;
 
@@ -11,21 +12,27 @@ impl Decoder for MqttCodec {
     type Error = io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<Packet>> {
+        // NOTE: `decode` might be called with `buf.len == 0` when prevous
+        // decode call read all the bytes in the stream. We should return
+        // Ok(None) in those cases or else the `read` call will return
+        // Ok(0) => translated to UnexpectedEOF by `byteorder` crate.
+        // `read` call Ok(0) happens when buffer specified was 0 bytes in len
+        // https://doc.rust-lang.org/std/io/trait.Read.html#tymethod.read
+        if buf.len() == 0 {
+            return Ok(None);
+        }
+
         let (packet, len) = {
             let mut buf_ref = buf.as_ref();
             match buf_ref.read_packet_with_len() {
                 Err(e) => {
-                    if let Error::Io(e) = e {
+                    if let mqtt3::Error::Io(e) = e {
                         match e.kind() {
                             ErrorKind::TimedOut | ErrorKind::WouldBlock => return Ok(None),
-                            _ => {
-                                println!("* {:?}", e);
-                                return Err(io::Error::new(ErrorKind::Other, "Invalid Mqtt Frame"));
-                            }
+                            _ => return Err(io::Error::new(e.kind(), e.description())),
                         }
                     } else {
-                        println!("** {:?}", e);
-                        return Err(io::Error::new(ErrorKind::Other, "Invalid Mqtt Frame"));
+                        return Err(io::Error::new(ErrorKind::Other, e.description()));
                     }
                 }
                 Ok(v) => v,
@@ -33,7 +40,8 @@ impl Decoder for MqttCodec {
         };
 
         buf.split_to(len);
-        println!("{:?}, {:?}", len, packet);
+
+        // println!("{:?}, {:?}, {:?}", len, packet, buf.len());
         Ok(Some(packet))
     }
 }
